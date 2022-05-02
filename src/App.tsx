a4b8d6d02a5c534d81d3835ac1d21f4f06b9d970
Anna Tveritnyeva <atveretniv@gmail.com>
2022-05-02 11:25:56 +0300

    Creates basic files and configurations for the new project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Footer from './components/layout/Footer';
+import Main from './components/layout/Main';
+import Navbar from './components/layout/Navbar';
 
-function App() {
+function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Main />
+      <Footer />
     </div>
   );
 }
