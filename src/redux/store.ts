11976a892722c2367b59a06c1ae6f2fa12ad8f0d
Anna Tveritnyeva <atveretniv@gmail.com>
2022-05-04 11:23:18 +0300

    Persists redux state on refresh

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,37 @@
-import {combineReducers, legacy_createStore as createStore} from "redux"
-import { GuestReducer } from "./reducers/GuestReducer";
-import { UserReducer } from "./reducers/UserReducer";
+import {combineReducers, EmptyObject, legacy_createStore as createStore, Store} from "redux"
+import { GuestReducer, GuestState } from "./reducers/GuestReducer";
+import { UserReducer, UserState } from "./reducers/UserReducer";
 
 const reducers = combineReducers({guest:GuestReducer, user: UserReducer})
+//see if there's anothe better, way to do that 
+//make state persist on refreshed
+function saveToLocalStorage(store: EmptyObject & {
+    guest: GuestState;
+    user: UserState;
+}) {
+    try {
+        const serializedStore = JSON.stringify(store);
+        window.localStorage.setItem('store', serializedStore);
+    } catch(e) {
+        console.log(e);
+    }
+}
 
-const store = createStore(reducers);
+function loadFromLocalStorage() {
+    try {
+        const serializedStore = window.localStorage.getItem('store');
+        if(serializedStore === null) return undefined;
+        return JSON.parse(serializedStore);
+    } catch(e) {
+        console.log(e);
+        return undefined;
+    }
+}
+
+const persistedState = loadFromLocalStorage();
+
+const store = createStore(reducers, persistedState);
+
+store.subscribe(() => saveToLocalStorage(store.getState()));
 
 export default store;
