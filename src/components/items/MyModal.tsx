e4d2aea862c91c153cb69bdaa1861e6bd8510a98
AnnaTveritnyeva <atveretniv@gmail.com>
2022-06-04 19:19:31 +0300

    Makes it use singleCard component as a template

--- src/components/items/MyModal.tsx
+++ src/components/items/MyModal.tsx
@@ -1,43 +1,58 @@
-import { Container, IconButton, Modal, Typography } from "@mui/material";
+import { Box, IconButton, Modal } from "@mui/material";
 import makeStyles from "@mui/styles/makeStyles";
 import { theme } from "../../theme";
 import ClearIcon from '@mui/icons-material/Clear';
+import SingleCard from "./SingleCard";
 
 const UseStyles = makeStyles({
     container: {
-        borderRadius: '5%',
-        height: "500px",
-        width: "500px",
-        backgroundColor: 'white',
+        width: "40vw",
+        [theme.breakpoints.down("md")]: {
+            width: '100vw'
+        },
         position: "absolute",
-        bottom: '0',
         left: '0',
-        top: '0',
         right: '0',
         margin: 'auto',
-        paddingInline: theme.spacing(10)
+        outline: 'none',
     },
 
 });
 
-
 interface MyModalProps {
-    open: boolean,
+    open: boolean
     close: any
-    content: JSX.Element;
+    content: JSX.Element
+    title: string
 }
 
 function MyModal(props: MyModalProps): JSX.Element {
     const classes = UseStyles();
 
     return (
-        <Modal open={props.open}>
-            <Container className={classes.container} sx={{ width: '50vw' }}>
-                <IconButton onClick={props.close}>
+        <Modal
+            open={props.open}
+            onSubmit={() => { props.close() }}
+        >
+            <Box
+                className={classes.container}
+                sx={{
+                    width: '60vw',
+                    [theme.breakpoints.down("sm")]: { width: '100vw' }
+                }}
+            >
+                <IconButton  
+                size="large"
+                color="primary" 
+                onClick={props.close}
+                >
                     <ClearIcon />
                 </IconButton>
-                {props.content}
-            </Container>
+                <SingleCard 
+                title={props.title} 
+                content={props.content} 
+                />
+            </Box>
         </Modal>
 
     )
