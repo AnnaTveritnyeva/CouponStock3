ad6cdf14d3a0c5f651d9ab6378902d75fcadac0e
AnnaTveritnyeva <atveretniv@gmail.com>
2022-06-06 14:46:18 +0300

    Adds inputfield style changes

--- src/components/items/JoinCompanyItem.tsx
+++ src/components/items/JoinCompanyItem.tsx
@@ -6,61 +6,61 @@ import SingleCard from "./SingleCard";
 const UseStyles = makeStyles({
     text: {
         marginBottom: theme.spacing(10)
-    },
-    inputField: {
-        marginTop: theme.spacing(1)
     }
 })
 
-
-
 function JoinCompanyItem(): JSX.Element {
-    
-    const InputField = (title: string, type: string, placeholder: string) => {
-        return <div className={classes.inputField}>
-            <Typography className={classes.text} variant="subtitle2" gutterBottom color={theme.palette.primary.light}>
-                {title}
-            </Typography>
-            <TextField type={type} placeholder={placeholder} fullWidth required />
-        </div>
+    const marginTop = { marginTop: theme.spacing(1) }
 
-    }
     const classes = UseStyles();
     return (
         <SingleCard
             title="Join Our Community"
             content={
                 <div>
-                    <Typography 
-                    className={classes.text} 
-                    variant="subtitle1" 
-                    align="center" 
-                    gutterBottom>
+                    <Typography
+                        className={classes.text}
+                        variant="subtitle1"
+                        align="center"
+                        gutterBottom>
                         Have your own buisness?
                         <br />
                         CouponStock gives an opportunity to every company submit their coupons for free.
                         <br />
                         Register now and start adding coupons!
-                        <br/>
+
                     </Typography>
 
-                    <Typography fontWeight={theme.typography.fontWeightBold} align="center" color="secondary" >
+                    <Typography fontWeight={theme.typography.fontWeightBold} align="center" sx={{ marginBlock: theme.spacing(2) }}>
                         Register:
                     </Typography>
 
-                    {InputField("company name:", "", "company")}
+                    <TextField
+                        label="company name" variant="outlined"
+                        placeholder={"company"} required
+                        sx={marginTop} fullWidth
+                    />
+                    <TextField
+                        label="email" type="email" variant="outlined"
+                        placeholder={"company@email.com"} required
+                        sx={marginTop} fullWidth
+                    />
+                    <TextField
+                        label="password" type="password"
+                        variant="outlined" required
+                        sx={marginTop} fullWidth
+                    />
+                    <TextField
+                        label="image" type="url" variant="outlined"
+                        placeholder={"http://image.jpg"} required
+                        sx={marginTop} fullWidth
+                    />
 
-                    {InputField("email:", "email", "company@email.com")}
-
-                    {InputField("password:", "password", "password")}
-
-                    {InputField("image:", "url", "http://image.jpg")}
-
-                    <Button 
-                    sx={{marginBlock: theme.spacing(2)}} 
-                    variant="contained" 
-                    fullWidth 
-                    size="large" 
+                    <Button
+                        sx={{ marginBlock: theme.spacing(2) }}
+                        variant="contained"
+                        fullWidth
+                        size="large"
                     >
                         Submit
                     </Button>
